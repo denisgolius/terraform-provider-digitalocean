1b7a875a4ec8be637781cbae530a7f05abd6b57f
Denis <denisgolius@users.noreply.github.com>
2019-02-11 15:32:32 +0200

    Add all zone + my zones

--- examples/droplet/variable.tf
+++ examples/droplet/variable.tf
@@ -4,8 +4,80 @@
 
 # As of 11-02-2018
 
+# Other zones
+# variable "do_lon1" {
+#   description = "Digital Ocean London Data Center 1"
+#   default     = "lon1"
+# }
+# 
+# variable "do_nyc1" {
+#   description = "Digital Ocean New York Data Center 1"
+#   default     = "nyc1"
+# }
+#
+# variable "do_nyc2" {
+#   description = "Digital Ocean New York Data Center 2"
+#   default     = "nyc2"
+# }
+#
+# variable "do_nyc3" {
+#   description = "Digital Ocean New York Data Center 3"
+#   default     = "nyc3"
+# }
+#
+# variable "do_ams2" {
+#   description = "Digital Ocean Amsterdam Data Center 2"
+#   default     = "ams2"
+# }
+#
+# variable "do_ams3" {
+#   description = "Digital Ocean Amsterdam Data Center 3"
+#   default     = "ams3"
+# }
+#
+# variable "do_sfo1" {
+#   description = "Digital Ocean San Francisco Data Center 1"
+#   default     = "sfo1"
+# }
+#
+# variable "do_sfo2" {
+#   description = "Digital Ocean San Francisco Data Center 2"
+#   default     = "sfo2"
+# }
+#
+# variable "do_sgp1" {
+#   description = "Digital Ocean Singapore Data Center 1"
+#   default     = "sgp1"
+# }
+#
+# variable "do_tor1" {
+#   description = "Digital Ocean Toronto Datacenter 1"
+#   default     = "tor1"
+# }
+#
+# variable "do_lon1" {
+#   description = "Digital Ocean London Data Center 1"
+#   default     = "lon1"
+# }
+#
+# variable "do_fra1" {
+#    description = "Digital Ocean Frankfurt Data Center 1"
+#   default     = "fra1"
+# }
+#
+# variable "do_tor1" {
+#   description = "Digital Ocean Toronto Datacenter 1"
+#   default     = "tor1"
+# }
+#
+# variable "do_blr1" {
+#   description = "Digital Ocean Bangalore Data Center 1"
+#   default     = "blr1"
+# }
+#
 #
 
+# For me
 variable "do_ams2" {
   description = "Digital Ocean Amsterdam Data Center 2"
   default     = "ams2"
@@ -19,11 +91,6 @@ variable "do_ams3" {
 variable "do_fra1" {
   description = "Digital Ocean Frankfurt Data Center 1"
   default     = "fra1"
-}
-
-variable "do_lon1" {
-  description = "Digital Ocean London Data Center 1"
-  default     = "lon1"
 }
 
 # Default Os
