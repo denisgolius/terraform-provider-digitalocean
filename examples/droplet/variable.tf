55aa9c6f7f1dcb634be1b7026d0eda24a083e725
Denis <denisgolius@users.noreply.github.com>
2019-02-11 14:08:38 +0200

    Delete not needed regions

--- examples/droplet/variable.tf
+++ examples/droplet/variable.tf
@@ -2,7 +2,7 @@
 
 # Current Available Datacenter Regions
 
-# As of 28-05-2017
+# As of 11-02-2018
 
 #
 
@@ -16,11 +16,6 @@ variable "do_ams3" {
   default     = "ams3"
 }
 
-variable "do_blr1" {
-  description = "Digital Ocean Bangalore Data Center 1"
-  default     = "blr1"
-}
-
 variable "do_fra1" {
   description = "Digital Ocean Frankfurt Data Center 1"
   default     = "fra1"
@@ -29,36 +24,6 @@ variable "do_fra1" {
 variable "do_lon1" {
   description = "Digital Ocean London Data Center 1"
   default     = "lon1"
-}
-
-variable "do_nyc1" {
-  description = "Digital Ocean New York Data Center 1"
-  default     = "nyc1"
-}
-
-variable "do_nyc2" {
-  description = "Digital Ocean New York Data Center 2"
-  default     = "nyc2"
-}
-
-variable "do_nyc3" {
-  description = "Digital Ocean New York Data Center 3"
-  default     = "nyc3"
-}
-
-variable "do_sfo1" {
-  description = "Digital Ocean San Francisco Data Center 1"
-  default     = "sfo1"
-}
-
-variable "do_sgp1" {
-  description = "Digital Ocean Singapore Data Center 1"
-  default     = "sgp1"
-}
-
-variable "do_tor1" {
-  description = "Digital Ocean Toronto Datacenter 1"
-  default     = "tor1"
 }
 
 # Default Os
